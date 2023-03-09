a91e50e7be3ed42ace738445210dc7ee8eeb6556
JoelPagliuca <JoelPagliuca@users.noreply.github.com>
2023-03-09 21:30:12 +1100

    make it look nicer

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ import (
 	"strings"
 )
 
+const (
+	DiscordWebhookURLKey = "DISCORD_WEBHOOK_URL"
+)
+
 type IssueEvent struct {
 	Action string `json:"action"`
 	Issue  struct {
@@ -21,26 +25,30 @@ func main() {
 	payloadPath := os.Getenv("GITHUB_EVENT_PATH")
 	payload, err := parsePayload(payloadPath)
 	if err != nil {
-		fmt.Printf("Error parsing payload: %v", err)
+		fmt.Printf("[-] error parsing payload: %v", err)
 		return
 	}
 
 	// Check if the event was a closed issue
 	if payload.Action != "closed" {
-		fmt.Printf("Not a closed issue event, action=%s\n", payload.Action)
+		fmt.Printf("[-] not a closed issue event, action=%s\n", payload.Action)
 		return
 	}
 
 	// Send the issue title to a Discord webhook
-	webhookURL := os.Getenv("DISCORD_WEBHOOK_URL")
-	if webhookURL != "" {
-		err = sendDiscordWebhook(webhookURL, payload.Issue.Title)
-		if err != nil {
-			fmt.Printf("Error sending Discord webhook: %v", err)
-			return
-		}
-		fmt.Println("Sent issue title to Discord!")
+	webhookURL := os.Getenv(DiscordWebhookURLKey)
+	if webhookURL == "" {
+		fmt.Printf("%s not set\n", DiscordWebhookURLKey)
+		os.Exit(1)
 	}
+
+	issueURL := fmt.Sprintf("https://github.com/%s/issues/%d", os.Getenv("GITHUB_REPOSITORY"), payload.Issue.Number)
+	err = sendDiscordWebhook(webhookURL, ":white_check_mark: completed: "+payload.Issue.Title+"\n"+issueURL)
+	if err != nil {
+		fmt.Printf("[-] error sending discord webhook: %v", err)
+		return
+	}
+	fmt.Println("[+] sent issue to discord")
 
 	// Print a message to the console
 	fmt.Printf("Issue #%d closed: %s\n", payload.Issue.Number, payload.Issue.Title)
@@ -50,14 +58,14 @@ func main() {
 func parsePayload(path string) (*IssueEvent, error) {
 	payloadFile, err := os.Open(path)
 	if err != nil {
-		return nil, fmt.Errorf("failed to open payload file: %v", err)
+		return nil, fmt.Errorf("[-] failed to open payload file: %v", err)
 	}
 	defer payloadFile.Close()
 
 	var payload IssueEvent
 	err = json.NewDecoder(payloadFile).Decode(&payload)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse payload file: %v", err)
+		return nil, fmt.Errorf("[-] failed to parse payload file: %v", err)
 	}
 
 	return &payload, nil
@@ -67,19 +75,21 @@ func parsePayload(path string) (*IssueEvent, error) {
 func sendDiscordWebhook(webhookURL string, message string) error {
 	// Create the payload data
 	payload := map[string]string{
-		"content": message,
+		"username":   "Github Issues",
+		"avatar_url": "https://cdn-icons-png.flaticon.com/512/25/25231.png",
+		"content":    message,
 	}
 
 	// Convert the payload to JSON
 	payloadJSON, err := json.Marshal(payload)
 	if err != nil {
-		return fmt.Errorf("failed to encode payload: %v", err)
+		return fmt.Errorf("[-] failed to encode payload: %v", err)
 	}
 
 	// Create a new POST request
 	req, err := http.NewRequest("POST", webhookURL, strings.NewReader(string(payloadJSON)))
 	if err != nil {
-		return fmt.Errorf("failed to create POST request: %v", err)
+		return fmt.Errorf("[-] failed to create POST request: %v", err)
 	}
 
 	// Set the content type to application/json
@@ -89,7 +99,7 @@ func sendDiscordWebhook(webhookURL string, message string) error {
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
-		return fmt.Errorf("failed to send POST request: %v", err)
+		return fmt.Errorf("[-] failed to send POST request: %v", err)
 	}
 	defer resp.Body.Close()
 
